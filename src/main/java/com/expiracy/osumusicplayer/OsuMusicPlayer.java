41823eb0788850f11efbc83f2bce873e869e10f8
James Gray <jameslgray.home@gmail.com>
2023-09-02 02:59:25 +0100

    shows playing song image, name, title some small style improvements

--- src/main/java/com/expiracy/osumusicplayer/OsuMusicPlayer.java
+++ src/main/java/com/expiracy/osumusicplayer/OsuMusicPlayer.java
@@ -9,49 +9,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Paint;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+import java.util.TreeMap;
 
 public class OsuMusicPlayer extends Application {
     protected Stage stage;
 
     protected Pane sideBox = new Pane();
     protected ScrollPane songsBox = new ScrollPane();
-    protected Pane bottomBox = new Pane();
+    protected Pane playingInfoBox = new Pane();
+    protected Pane playerBox = new Pane();
 
     protected Button configureButton = new Button("Configure");
     protected Button homeButton = new Button("Home");
     protected Button searchButton = new Button("Search");
 
-    Map<Integer, Song> songs = new HashMap<>();
-
-    public OsuMusicPlayer() {
-        this.sideBox.getStyleClass().add("side-box");
-        this.songsBox.getStyleClass().addAll("songs-box", "edge-to-edge");
-        this.bottomBox.getStyleClass().add("bottom-box");
-
-        this.homeButton.getStyleClass().add("side-button");
-        this.searchButton.getStyleClass().add("side-button");
-        this.configureButton.getStyleClass().add("side-button");
-
-        this.songsBox.setFocusTraversable(false);
-
-        this.homeButton.setOnAction(home);
-        this.searchButton.setOnAction(search);
-        this.configureButton.setOnAction(configure);
-
-        VBox sideButtons = new VBox();
-        sideButtons.getChildren().addAll(this.searchButton, this.homeButton, this.configureButton);
-        this.sideBox.getChildren().add(sideButtons);
-    }
+    Map<Integer, Song> songs = new TreeMap<>();
 
     private final EventHandler<ActionEvent> configure = event -> {
         DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -68,9 +48,19 @@ public class OsuMusicPlayer extends Application {
 
     private final EventHandler<ActionEvent> playSong = event -> {
         Button button = (Button) event.getSource();
+        Song song = this.songs.get(Integer.valueOf(button.getId()));
 
-        System.out.println(this.songs.get(Integer.valueOf(button.getId())).getTitle());
+        ImageView image = (ImageView) this.playingInfoBox.lookup("#playingImage");
+        image.setImage(new Image(song.getImage().getAbsolutePath()));
+
+        Label title = (Label) this.playingInfoBox.lookup("#playingTitle");
+        title.setText(song.getTitle());
+
+        Label artist = (Label) this.playingInfoBox.lookup("#playingArtist");
+        artist.setText(song.getArtist());
+
     };
+
 
     private final EventHandler<ActionEvent> search = event -> {
         System.out.println("Search");
@@ -85,11 +75,56 @@ public class OsuMusicPlayer extends Application {
     public void start(Stage stage) {
         this.stage = stage;
 
+        this.sideBox.getStyleClass().add("side-box");
+        this.songsBox.getStyleClass().addAll("songs-box", "edge-to-edge");
+        this.songsBox.getStylesheets().add(getClass().getResource("scroll-bar.css").toExternalForm());
+        this.playingInfoBox.getStyleClass().add("playing-info-box");
+        this.playerBox.getStyleClass().add("player-box");
+
+        this.homeButton.getStyleClass().add("side-button");
+        this.searchButton.getStyleClass().add("side-button");
+        this.configureButton.getStyleClass().add("side-button");
+        this.songsBox.setFocusTraversable(false);
+
+        this.homeButton.setOnAction(home);
+        this.searchButton.setOnAction(search);
+        this.configureButton.setOnAction(configure);
+
+        VBox sideButtons = new VBox();
+        sideButtons.getChildren().addAll(this.searchButton, this.homeButton, this.configureButton);
+        this.sideBox.getChildren().add(sideButtons);
+
+        VBox artistAndTitle = new VBox();
+
+        Label title = new Label("Title");
+        title.setId("playingTitle");
+        title.getStyleClass().add("title");
+        title.setWrapText(true);
+
+        Label artist = new Label("Artist");
+        artist.setId("playingArtist");
+        artist.getStyleClass().add("artist");
+        artist.setWrapText(true);
+
+        artistAndTitle.getChildren().addAll(title, artist);
+        artistAndTitle.setStyle("-fx-alignment: center-left;");
+
+        ImageView image = new ImageView("C:\\Users\\james\\Documents\\.Code\\osuMusicPlayer2\\src\\main\\resources\\com\\expiracy\\osumusicplayer\\image-placeholder.jpg");
+        image.setFitHeight(43);
+        image.setFitWidth(77);
+        image.setId("playingImage");
+
+        HBox songInfo = new HBox();
+        songInfo.getChildren().addAll(image, artistAndTitle);
+        songInfo.getStyleClass().add("playing-info-box");
+
+        this.playingInfoBox.getChildren().addAll(songInfo);
+
         GridPane layoutPane = new GridPane();
         layoutPane.setHgap(10);
         layoutPane.setStyle("-fx-background-color: black");
 
-        ColumnConstraints column1 = new ColumnConstraints(250);
+        ColumnConstraints column1 = new ColumnConstraints(300);
         ColumnConstraints column2 = new ColumnConstraints();
         column2.setHgrow(javafx.scene.layout.Priority.ALWAYS);
 
@@ -103,7 +138,8 @@ public class OsuMusicPlayer extends Application {
 
         layoutPane.add(this.sideBox, 0, 0);
         layoutPane.add(this.songsBox, 1, 0);
-        layoutPane.add(this.bottomBox, 0, 1, 2, 1);
+        layoutPane.add(this.playerBox, 1, 1);
+        layoutPane.add(this.playingInfoBox, 0, 1);
 
         layoutPane.getStyleClass().addAll("background-box", "background-boxes");
 
@@ -119,29 +155,39 @@ public class OsuMusicPlayer extends Application {
     public void displaySongs(Map<Integer, Song> songs) {
         VBox songButtons = new VBox();
 
+        Integer songNumber = 1;
+
         for (Map.Entry<Integer, Song> entry : songs.entrySet()) {
             Integer songId = entry.getKey();
             Song song = entry.getValue();
 
             Button songButton = new Button();
 
-            VBox labels = new VBox();
+            HBox songInfo = new HBox();
+            Label songNumberLabel = new Label(songNumber.toString());
+            songNumberLabel.getStyleClass().add("song-number");
+
+            VBox titleAndArtist = new VBox();
             Label title = new Label(song.getTitle());
             title.getStyleClass().add("title");
 
             Label artist = new Label(song.getArtist());
             artist.getStyleClass().add("artist");
 
-            labels.getChildren().addAll(title, artist);
+            titleAndArtist.getChildren().addAll(title, artist);
+
+            songInfo.getChildren().addAll(songNumberLabel, titleAndArtist);
 
             songButton.getStyleClass().add("song-button");
-            songButton.setGraphic(labels);
+            songButton.setGraphic(songInfo);
 
             songButton.setId(songId.toString());
             songButton.setOnAction(playSong);
             //ImageView imageView = new ImageView(song.getImage().toURI().toString());
 
             songButtons.getChildren().add(songButton);
+
+            ++songNumber;
         }
 
         this.songsBox.setContent(songButtons);
